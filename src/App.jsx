0e25fb3746380446308279223a9427fd160ec582
sagarsinghthakur002 <sagarsinghthakur002@gmail.com>
2024-11-07 13:02:35 +0545

    feat: hero

--- src/App.jsx
+++ src/App.jsx
@@ -1,31 +1,32 @@
-import React from 'react'
-import Header from './components/Header'
-import Popural from './Section/Popural'
-import Hero from './Section/Hero' 
-import About from './Section/About'
-import Popural from './Section/Popural'
-import Properties from './Section/Properties'
-import Service from './Section/Service'
-import Clients from './Section/Clients'
-import Contact from './Section/Contact'
-import Footer from './components/Footer'
-import { DarkModeProvider } from './components/DarkModeContext'
+import React from 'react';
+import Header from './components/Header';
+import Hero from './Section/Hero';
+import About from './Section/About';
+import Popural from './Section/Popural';
+import Properties from './Section/Properties';
+import Service from './Section/Service';
+import Clients from './Section/Clients';
+import Contact from './Section/Contact';
+import Footer from './components/Footer';
+import { DarkModeProvider } from './components/DarkModeContext';
+
 const App = () => {
   return (
     <>
+
     <DarkModeProvider>
-      <Header/>
-      <Hero/>
-      <About/>
-      <Popural/>
-      <Properties/>
-      <Service/>
-      <Clients/>
-      <Contact/>
-      <Footer/>
+      <Header />
+      <Hero />
+      <About />
+      <Popural />
+      <Properties />
+      <Service />
+      <Clients />
+      <Contact />
+      <Footer />
     </DarkModeProvider>
     </>
   )
-}
+};
 
-export default App
+export default App;
--- src/Section/Hero.jsx
+++ src/Section/Hero.jsx
@@ -1,12 +1,32 @@
-import React from 'react'
-import { useDarkMode} from '../components/DarkModeContext'
-import Aos from 'aos'
-const Hero = () => {
-  return (
-    <div>
-      
-    </div>
-  )
-}
+import React, { useEffect } from 'react';
+import { useDarkMode } from '../components/DarkModeContext';
+import heroimg from '../assets/images/hero1.webp';
+import Aos from 'aos';
+import 'aos/dist/aos.css';
 
-export default Hero
+const Hero = () => {
+    useEffect(() => {
+        Aos.init({
+            offset: 200,
+            duration: 800,
+            easing: 'ease-in-out',
+            delay: 100,
+        });
+    }, []);
+
+    const { darkMode } = useDarkMode(); 
+
+    return (
+        <div className={darkMode ? 'dark bg-black' : 'light bg-white'}>
+            <section
+                id="hero"
+                className="w-[95%] h-[600px] m-auto bg-cover bg-center rounded-xl flex justify-center flex-col items-start lg:px-28 px-10 gap-7 z-20"
+                style={{ backgroundImage: `url(${heroimg})` }}
+            >
+            
+            </section>
+        </div>
+    );
+};
+
+export default Hero;
